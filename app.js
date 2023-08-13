e50f7ac26b679bfcebcd6b7c4f769af8bc8b151c
Eidan87 <42222419+Eidan87@users.noreply.github.com>
2023-08-13 23:45:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const responsesModule = require('./responses');
 const elogiosModule = require('./elogios');
 const learnedResponsesModule = require('./learnedResponses');
-const moodResponseModule = require('./modoRespuesta'); 
+const moodResponseModule = require('./modoRespuesta');
 const contarChistesModule = require('./chistes');
 
 const app = express();
@@ -15,61 +15,80 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 let conversationHistory = [];
 
+// Función para manejar el mensaje de bienvenida
+function handleWelcomeMessage() {
+  return "¡Bienvenido a nuestro chat de elogios! En este chat, puedes interactuar con el bot de elogios. Algunas cosas que puedes hacer:\n\n" +
+    "- Envía mensajes para conversar con el bot.\n" +
+    "- Usa comandos como '/historial' para ver el historial de conversaciones.\n" +
+    "- Pide un elogio personalizado escribiendo 'elogio' seguido de tu elogio.\n" +
+    "- Si necesitas ayuda, simplemente escribe 'ayuda'.\n" +
+    "- Si quieres escuchar un chiste, solo escribe 'chiste'.";
+}
+
+// Función para manejar mensajes de ayuda
+function handleHelpMessage() {
+  return 'Puedes escribir mensajes o usar comandos como /historial para ver el historial de conversaciones.';
+}
+
+// Función para manejar el historial de conversaciones
+function handleHistoryMessage() {
+  if (conversationHistory.length === 0) {
+    return 'No hay conversaciones anteriores.';
+  } else {
+    let reply = 'Historial de conversaciones:\n';
+    conversationHistory.forEach((message, index) => {
+      reply += `${index + 1}. Tú: ${message.userMessage}\n`;
+      reply += `   Bot: ${message.botReply}\n`;
+    });
+    return reply;
+  }
+}
+
+// Función para manejar mensajes de elogio personalizado
+function handleElogioMessage(userMessage) {
+  const elogioPersonalizado = userMessage.slice('elogio'.length).trim();
+  if (elogioPersonalizado) {
+    return `¡${elogioPersonalizado}!`;
+  } else {
+    return elogiosModule.getRandomElogio();
+  }
+}
+
 app.post('/chat', async (req, res) => {
   const userMessage = req.body.message;
   let reply = '';
 
-  // Manejar el mensaje de bienvenida
   if (userMessage === 'welcome') {
-    reply = "¡Bienvenido a nuestro chat de elogios! En este chat, puedes interactuar con el bot de elogios. Algunas cosas que puedes hacer:\n\n" +
-            "- Envía mensajes para conversar con el bot.\n" +
-            "- Usa comandos como '/historial' para ver el historial de conversaciones.\n" +
-            "- Pide un elogio personalizado escribiendo 'elogio' seguido de tu elogio.\n" +
-            "- Si necesitas ayuda, simplemente escribe 'ayuda'.\n" +
-            "- Si quieres escuchar un chiste, solo escribe 'chiste'.";
+    reply = handleWelcomeMessage();
+  } else if (userMessage.startsWith('ayuda')) {
+    reply = handleHelpMessage();
+  } else if (userMessage.startsWith('historial')) {
+    reply = handleHistoryMessage();
+  } else if (userMessage.startsWith('elogio')) {
+    reply = handleElogioMessage(userMessage);
   } else {
-    if (userMessage.startsWith('ayuda')) {
-      reply = 'Puedes escribir mensajes o usar comandos como /historial para ver el historial de conversaciones.';
-    } else if (userMessage.startsWith('historial')) {
-      if (conversationHistory.length === 0) {
-        reply = 'No hay conversaciones anteriores.';
-      } else {
-        reply = 'Historial de conversaciones:\n';
-        conversationHistory.forEach((message, index) => {
-          reply += `${index + 1}. Tú: ${message.userMessage}\n`;
-          reply += `   Bot: ${message.botReply}\n`;
-        });
-      }
-    } else if (userMessage.startsWith('elogio')) {
-      const elogioPersonalizado = userMessage.slice('elogio'.length).trim();
-      if (elogioPersonalizado) {
-        reply = `¡${elogioPersonalizado}!`;
-      } else {
-        reply = elogiosModule.getRandomElogio();
-      }
+    const customResponse = responsesModule.findResponse(userMessage);
+
+    if (customResponse) {
+      reply = customResponse;
     } else {
-      const customResponse = responsesModule.findResponse(userMessage);
-
-      if (customResponse) {
-        reply = customResponse;
-      } else {
-        const randomElogio = elogiosModule.getRandomElogio();
-        reply = randomElogio;
-      }
-
-      // Intentamos obtener una respuesta basada en el estado de ánimo del usuario
-      const moodResponse = moodResponseModule.getMoodResponse(userMessage);
-      if (moodResponse) {
-        reply = moodResponse;
-      } else if (userMessage.includes('chiste')) {
-        const randomJoke = contarChistesModule.getRandomJoke();
-        reply = randomJoke;
-      }
-
-      conversationHistory.push({ userMessage, botReply: reply });
-      res.json({ reply });
+      const randomElogio = elogiosModule.getRandomElogio();
+      reply = randomElogio;
     }
+
+    const moodResponse = moodResponseModule.getMoodResponse(userMessage);
+    if (moodResponse) {
+      reply = moodResponse;
+    } else if (userMessage.includes('chiste')) {
+      const randomJoke = contarChistesModule.getRandomJoke();
+      reply = randomJoke;
+    }
+
+    conversationHistory.push({ userMessage, botReply: reply });
   }
+
+  // Send the bot's reply back to the client
+  res.json({ reply });
 });
 
 app.listen(port, () => {
